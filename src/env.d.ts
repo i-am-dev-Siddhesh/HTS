8da11ef9ff642effa475c114d45b0356551f7bec
siddhesh shinde <shindesiddhesh.co.in@gmail.com>
2024-05-28 13:48:51 +0530

    feat: type fixes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,20 +1,21 @@
-import { Session } from "express-session";
+import { Session } from 'express-session';
+import { IUser } from './utils/types';
 
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     API_KEY: string;
     CLIENT_URL: string;
-    ADMIN_URL: string;
     DATABASE_URL: string;
-    SESSION_SECRET: string;
-    REDIS_URL: string;
-    COOKIE_DOMAIN: string;
+    JWT_TOKEN_SECRET: string;
+    DOMAIN: string;
+    SERVER_ENV: string;
   }
 }
 
 declare namespace Express {
   export interface Request {
     session: Session;
+    user: IUser;
   }
 }
